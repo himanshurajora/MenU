f69d05badbaf9226e87d2bc1fb6220ffdc321b8b
Himanshu Jangid <iamahalofan@gmail.com>
2021-08-12 22:49:48 +0530

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
+import { useSpring, animated } from 'react-spring'
+import Tilt from 'react-parallax-tilt'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const fadein = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
+
   return (
     <div className="App">
-    
+      <h1 id="welcome">Welcome to MenU <br /> - By Himanshu</h1>
+      <div className="container">
+        <Tilt className="box" tiltMaxAngleX={10} tiltMaxAngleY={10} glareEnable={true} glareMaxOpacity={400}>
+            <h1>Are You Ready?</h1>
+            <button><strong>Start A <span id="menu">Call 📞</span></strong></button>
+        </Tilt>
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+      </div>
     </div>
   )
 }
