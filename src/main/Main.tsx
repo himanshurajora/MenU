703c3d33587bd5fee3b5ca0b721dd48fed79086c
Himanshu Jangid <iamahalofan@gmail.com>
2021-08-18 14:09:08 +0530

    chat done

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,42 +1,102 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useSpring, animated } from 'react-spring'
 import './Main.css'
-import io from 'socket.io-client'
+import io, { Socket } from 'socket.io-client'
 import Peer from 'peerjs'
 
 
+const roomId = new URL(window.location.toString()).pathname.slice(1, window.location.toString().length - 1)
+var username = localStorage.getItem('username')
+
 function Main() {
 
-    var localStream: MediaStream
-    var remoteStream: MediaStream
+    const [mypeer, setmypeer] = useState(
+        new Peer(undefined, {
+            "host": "/",
+            "port": 3005
+        }));
+
+    const [socket, setsocket] = useState(io("http://localhost:5000"))
 
     const peopleSec = useRef<HTMLDivElement>(null)
     const chatSec = useRef<HTMLDivElement>(null)
+    const chatBtn = useRef<HTMLButtonElement>(null)
+    const peopleBtn = useRef<HTMLButtonElement>(null)
 
+    const [message, setmessage] = useState("");
+
+    const showPeople = () => {
+        peopleSec.current.style.display = "block"
+        peopleBtn.current.className = "is-active"
+        chatBtn.current.className = "chatbtn"
+        chatSec.current.style.display = "none"
+    }
+
+    const showChat = () => {
+        peopleSec.current.style.display = "none"
+        chatSec.current.style.display = "block"
+        peopleBtn.current.className = "peoplebtn"
+        chatBtn.current.className = "is-active"
+    }
+
+    const addPerson = (username: string) => {
+        var person = document.createElement("div")
+        person.className = "person"
+        person.innerText = username
+        peopleSec.current.appendChild(person)
+
+        var message = document.createElement("div")
+        message.className = "person"
+        message.innerText = username + " joined the room"
+        chatSec.current.appendChild(message)
+    }
+
+    const addMessage = (username: string, message: string) => {
+        var msg = document.createElement("div")
+        msg.className = "text"
+        var user = document.createElement("p")
+        var text = document.createElement("p")
+        user.className = "title"
+        user.innerText = username
+        text.innerText = message
+        msg.append(user, text)
+        chatSec.current.appendChild(msg)
+    }
+
+    const sendMessage = (e: any) => {
+        e.preventDefault()
+        if (message) {
+            chatBtn.current.click()
+            socket.emit("message", roomId, username, message)
+            addMessage("You", message)
+        }
+        setmessage("")
+    }
 
     useEffect(() => {
-
-        const peopleSec = document.querySelector(".peopleSec")
-        const roomId = new URL(window.location.toString()).pathname.slice(1, window.location.toString().length - 1)
-        var username = localStorage.getItem('username')
-
-        var socket = io("http://localhost:5000")
-
-        var mypeer = new Peer(undefined, {
-            "host": "/",
-            "port": 3005
-        })
+        peopleSec.current.style.display = "block"
+        peopleBtn.current.className = "is-active"
+        chatSec.current.style.display = "none"
 
         mypeer.on('open', (id) => {
             socket.emit("joined", roomId, id, username)
         })
 
         socket.on("user-connected", (userid, username) => {
-            var person = document.createElement("div")
-            person.className = "person"
-            person.innerText = username
-            peopleSec.appendChild(person)
+            addPerson(username)
+            
+            navigator.mediaDevices.getUserMedia({"video": false, "audio": true}).then(stream=>{
+                
+            })
+
         })
+
+        socket.on("message", (username, message) => {
+            console.log("message came");
+
+            addMessage(username, message)
+        })
+
         // navigator.mediaDevices.getDisplayMedia({ video: true, audio: false }).then(stream => {
         //     me.current.srcObject = stream
         // })
@@ -65,43 +125,25 @@ function Main() {
         //     // })
         // })
 
-        // socket.on("user-connected", (userId) => {
 
-        //     var call = mypeer.call(userId, me.current.srcObject as MediaStream)
-
-        //     // call.on('stream', (stream) => {
-        //     //     you.current.srcObject = stream
-        //     // })
-
-        //     // call.on("close", () => {
-        //     //     you.current.srcObject = null
-        //     // })
-
-        // })
-
-    }, [])
-
-
-
-    const showPeople = () => {
-      
-    }
-
-    const showChat = () => {
-      
-    }
+    }, [socket])
 
     return (
         <div className="Main">
             <div className="container">
-                <div className="left"></div>
+                <div className="left">
+                    <div className="people-grid">
+                        <div className="person-circle">
+                        </div>
+                    </div>
+                </div>
                 <div className="right">
                     <div className="top">
                         <div className="people">
-                            <button id="people" onClick={showPeople}>People</button>
+                            <button className="peoplebtn" ref={peopleBtn} onClick={showPeople}>People</button>
                         </div>
                         <div className="chat">
-                            <button id="chat" onClick={showChat}>Chat</button>
+                            <button className="chatbtn" ref={chatBtn} onClick={showChat}>Chat</button>
                         </div>
                     </div>
                     <div className="bottom">
@@ -109,11 +151,14 @@ function Main() {
 
                         </div>
                         <div className="chatSec" ref={chatSec}>
-                            <div className="text">
-                                <p className="title">Himanshu Jangid</p>
-                            Demo Text</div>
-                        </div>
 
+                        </div>
+                        <div className="messageform">
+                            <form onSubmit={sendMessage}>
+                                <input type="text" autoComplete={"off"} id="messagetext" value={message} onChange={(e) => { setmessage(e.target.value) }} placeholder="Enter Message" />
+                                <input type="submit" value="Send" id="messagebtn" />
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
